50274a2da0b099c9ab9ddf7b267bef6f66de7959
josdem <joseluis.delacruz@gmail.com>
2016-03-20 17:19:01 -0600

    Agregando los metodos para obtener la session en la aplicacion

--- homework-3/server.js
+++ homework-3/server.js
@@ -4,9 +4,22 @@ var mongoose = require('mongoose')
 var bodyParser = require('body-parser')
 var uuid = require('uuid')
 
+var session = require('express-session')
+var MongoStore = require('express-session-mongo')
+var flash = require('flash')
+
 var Schema = mongoose.Schema
 
 mongoose.connect('mongodb://localhost/homework-2')
+
+var userSchema = Schema({
+  username: String,
+  displayName: String,
+  password: String,
+  uuid : {type: String, default: uuid.v4}
+})
+
+var User = mongoose.model('User', userSchema)
 
 var placeSchema = new Schema({
   name: String,
@@ -20,21 +33,30 @@ var Place = mongoose.model('Place', placeSchema)
 
 var application = express()
 
+  application.use(session({
+    secret: 'keyboard cat',
+    store: new MongoStore(),
+    saveUninitialized: true,
+    resave: true
+  }))
+
+application.use( flash() )
+
 application.engine('html', swig.renderFile)
 application.set('view engine', 'html')
 application.set('views', __dirname + '/views')
 
-application.set('view cache', false)
+  application.set('view cache', false)
 swig.setDefaults({cache:false})
 
 application.use( bodyParser.urlencoded({ extended:false }) )
-application.use('/assets', express.static('public'))
+  application.use('/assets', express.static('public'))
 
-application.get('/', function (request, response) {
-  response.render("index", {
-    title: "AirBnb"
+  application.get('/', function (request, response) {
+    response.render("index", {
+      title: "AirBnb"
+    })
   })
-})
 
 application.get('/listing', function (request, response) {
   response.render("listing", {
@@ -91,11 +113,11 @@ application.post('/city/:city/:uuid/description', function(request, response){
     }
 
     place.description = request.body.description
-    place.save(function(error){
-      if(error){
-        return response.send(500, 'I could not save place')
-      }
-    })
+      place.save(function(error){
+        if(error){
+          return response.send(500, 'I could not save place')
+        }
+      })
 
     response.render("advertise", {
       title: 'Advertise',
@@ -120,7 +142,59 @@ application.post('/city/:city/:uuid/remove', function(request, response){
   })
 })
 
+application.get('/log-in', function (req, res){
+  var error = res.locals.flash.pop()
+
+    res.render('log-in',{
+      error: error
+    })
+})
+
+
+application.get('/sign-up', function (req, res){
+  var error = res.locals.flash.pop()
+
+    res.render('sign-up', {
+      error: error
+    })
+})
+
+application.post('/sign-up', function (req, res){
+  if(!req.body.username || !req.body.password){
+    req.flash('sign-up-error', 'To sign up you need a username and a password')
+      return res.redirect('/sign-up')
+  }
+
+  User.findOne({username: req.body.username}, function(err, doc){
+    if(err){
+      return res.send(500, 'Internal Server Error')
+    }
+
+    if(doc){
+      req.flash('sign-up-error', 'Username is taken')
+        return res.redirect('/sign-up')
+    }
+
+    bcrypt.hash(req.body.password, null/* Salt */, null, function(err, hashedPassword) {
+      if(err){
+        return res.send(500, 'Internal Server Error')
+      }
+
+      User.create({
+        username: req.body.username,
+        password: hashedPassword
+      }, function(err, doc){
+        if(err){
+          return res.send(500, 'Internal Server Error')
+        }
+
+        req.session.userId = doc.uuid
+          res.redirect('/')
+      })
+    });
+  })
+})
 
 application.listen(3000, function () {
-   console.log('Example application listening on port 3000!')
+  console.log('Example application listening on port 3000!')
 })
